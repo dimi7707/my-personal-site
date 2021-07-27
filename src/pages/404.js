2e5e89d13fd0e81aa06c70ca9f62a70f9f317b71
Dimitri Avila <davila@rootstack.com>
2021-07-27 17:43:10 -0500

    feat: working on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,20 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import mainImage from '../images/image-404.jpg';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
+
+  console.log(mainImage);
+
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <div className="container mx-auto">
+      <img src={mainImage} alt="404" />
+      <div className="text-center p-8">
+        <a href="/" className="rounded-lg rounded-xl border-4 border-indigo-800 py-4 px-6 hover:text-white hover:bg-indigo-800">
+          Back to home
+        </a>
+      </div>
+    </div>
   )
 }
 
-export default NotFoundPage
+export default NotFoundPage;
