45a34b8b04da4f7e7dd46e2cefed0b85b8dcb089
Dimitri Avila <davila@rootstack.com>
2021-08-16 16:51:56 -0500

    fix: contact block

--- src/components/shared/organisms/ContactBlock.tsx
+++ src/components/shared/organisms/ContactBlock.tsx
@@ -18,8 +18,11 @@ export const ContactBlock = () => {
         <div className="text-xl text-center lg:px-60 py-4">
           <h3 className="text-3xl py-4"> Contact me </h3>
           <p>
-            If you want to talk about a job, project or just want to make a comment, share an idea. You can write to me through these means:
+            If you want to talk about a job, project or just want to make a comment, share an idea. You can write to me:
           </p>
+          <p className="font-sans text-2xl italic">
+            dimi7707@gmail.com / d_avila_07@hotmail.com
+        </p>
         </div>
       </div>
     </div>
