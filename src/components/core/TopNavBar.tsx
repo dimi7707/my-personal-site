32ee24f7900e2e1f858e5357e4a097db4f9a16d9
Dimitri Avila <davila@rootstack.com>
2022-01-25 16:07:34 -0500

    Refactor: cleaning code

--- src/components/core/TopNavBar.tsx
+++ src/components/core/TopNavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Disclosure } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
